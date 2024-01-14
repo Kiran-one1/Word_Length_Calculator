52abf993a9f63d29722150e12f5f318a9216d32f
Kiran-one1 <k.kiran621@gmail.com>
2024-01-14 18:36:03 +0530

    Initial commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,22 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String inputWord = scanner.nextLine();
+
+        int wordLength = calculateWordLength(inputWord);
+
+        System.out.println("Length of the entered word: " + wordLength);
+
+        scanner.close();
+    }
+
+    private static int calculateWordLength(String word) {
+        return word.length();
     }
 }
